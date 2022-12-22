64a43266177dbc6c4d2ff74dac0ac48831894a58
adarsht00001 <adarsht00001@gmail.com>
2022-12-22 13:18:12 +0530

    view post completed

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,8 +1,22 @@
 import React,{useEffect,useState,useContext} from 'react';
 import {PostContext} from '../../context/postContext'
+import { FirebaseContext } from '../../context/Context'
+import { collection, query, where, getFirestore,getDocs } from "firebase/firestore"
 import './View.css';
 function View() {
+  const [userDetails,setUserDetails]=useState('')
   const {postDeatils}=useContext(PostContext)
+  const {firebase} =useContext(FirebaseContext)
+  const db = getFirestore(firebase);
+  useEffect(()=>{
+    const {userId}=postDeatils
+    const q = query(collection(db, "users"), where("id", "==", userId))
+    getDocs(q).then((res)=>{
+      res.forEach((doc)=>{
+        setUserDetails(doc.data())
+      })
+    })
+  },[])
   return (
     <div className="viewParentDiv">
       <div className="imageShowDiv">
@@ -14,15 +28,15 @@ function View() {
       <div className="rightSection">
         <div className="productDetails">
           <p>&#x20B9;{postDeatils.price} </p>
-          <span>YAMAHA R15V3</span>
-          <p>Two Wheeler</p>
-          <span>Tue May 04 2021</span>
+          <span>{postDeatils.name}</span>
+          <p>{postDeatils.category}</p>
+          <span>{postDeatils.createdAt}</span>
         </div>
-        <div className="contactDetails">
+       {userDetails && <div className="contactDetails">
           <p>Seller details</p>
-          <p>No name</p>
-          <p>1234567890</p>
-        </div>
+          <p>{userDetails.username}</p>
+          <p>{userDetails.phone}</p>
+        </div>}
       </div>
     </div>
   );
